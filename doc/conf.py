49d6f6cdbf20b904fb53eff16aa036ff649e0e63
Duane Malcolm <duane.malcolm@gmail.com>
2012-02-15 09:31:27 +1300

    Sphinx sys.path for morphic on readthedocs.org

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.abspath('../morphic'))
+sys.path.append('../morphic')
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
