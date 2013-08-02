4e85d6ce9d1d4398b47624b4984fc6094d2708de
Duane Malcolm <duane.malcolm@gmail.com>
2013-08-02 16:24:47 +1200

    Changing mesh.userdata to mesh.metadata

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -16,120 +16,120 @@ class TestNode(unittest.TestCase):
 
     def test_init_attributes(self):
         mesh = mesher.Mesh()
-        self.assertTrue(isinstance(mesh.userdata, mesher.Metadata))
+        self.assertTrue(isinstance(mesh.metadata, mesher.Metadata))
     
     def test_set_attribute(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.age, 23)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.age, 23)
     
     def test_set_item(self):
         mesh = mesher.Mesh()
-        mesh.userdata['age'] = 23
-        self.assertEqual(mesh.userdata.age, 23)
+        mesh.metadata['age'] = 23
+        self.assertEqual(mesh.metadata.age, 23)
     
     def test_set_method(self):
         mesh = mesher.Mesh()
-        mesh.userdata.set('age', 23)
-        self.assertEqual(mesh.userdata.age, 23)
+        mesh.metadata.set('age', 23)
+        self.assertEqual(mesh.metadata.age, 23)
     
     def test_get_attribute(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.age, 23)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.age, 23)
     
     def test_get_item(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata['age'], 23)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata['age'], 23)
     
     def test_get_method(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.get('age'), 23)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.get('age'), 23)
     
     def test_delete_method(self):
         mesh = mesher.Mesh()
-        self.assertFalse('age' in mesh.userdata.keys())
-        mesh.userdata.age = 23
-        self.assertTrue('age' in mesh.userdata.keys())
-        mesh.userdata.delete('age')
-        self.assertFalse('age' in mesh.userdata.keys())
+        self.assertFalse('age' in mesh.metadata.keys())
+        mesh.metadata.age = 23
+        self.assertTrue('age' in mesh.metadata.keys())
+        mesh.metadata.delete('age')
+        self.assertFalse('age' in mesh.metadata.keys())
     
     def test_delete_method_nonexistant(self):
         mesh = mesher.Mesh()
-        mesh.userdata.delete('age')
+        mesh.metadata.delete('age')
     
     def test_get_method_nonexistant(self):
         mesh = mesher.Mesh()
-        age = mesh.userdata.get('age')
+        age = mesh.metadata.get('age')
         self.assertEqual(age, None)
         
     def test_get_method_nonexistant_default(self):
         mesh = mesher.Mesh()
-        age = mesh.userdata.get('age', 25)
+        age = mesh.metadata.get('age', 25)
         self.assertEqual(age, 25)
     
     def test_get_method_existant_default(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        age = mesh.userdata.get('age', 25)
+        mesh.metadata.age = 23
+        age = mesh.metadata.get('age', 25)
         self.assertEqual(age, 23)
     
     def test_set_method_overwrite(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.age, 23)
-        result = mesh.userdata.set('age', 25)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.age, 23)
+        result = mesh.metadata.set('age', 25)
         self.assertTrue(result)
-        self.assertEqual(mesh.userdata.age, 25)
+        self.assertEqual(mesh.metadata.age, 25)
         
     def test_set_method_no_overwrite(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.age, 23)
-        result = mesh.userdata.set('age', 25, False)
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.age, 23)
+        result = mesh.metadata.set('age', 25, False)
         self.assertFalse(result)
-        self.assertEqual(mesh.userdata.age, 23)
+        self.assertEqual(mesh.metadata.age, 23)
         
     def test_contains(self):
         mesh = mesher.Mesh()
-        mesh.userdata.age = 23
-        self.assertTrue('age' in mesh.userdata)
-        self.assertFalse('height' in mesh.userdata)
+        mesh.metadata.age = 23
+        self.assertTrue('age' in mesh.metadata)
+        self.assertFalse('height' in mesh.metadata)
     
     def test_length(self):
         mesh = mesher.Mesh()
-        mesh.userdata.name = 'Joe Bloggs'
-        mesh.userdata.age = 23
-        self.assertEqual(len(mesh.userdata), 2)
+        mesh.metadata.name = 'Joe Bloggs'
+        mesh.metadata.age = 23
+        self.assertEqual(len(mesh.metadata), 2)
         
     def test_data_types(self):
         mesh = mesher.Mesh()
-        mesh.userdata.name = 'Joe Bloggs'
-        self.assertEqual(mesh.userdata.name, 'Joe Bloggs')
-        mesh.userdata.age = 23
-        self.assertEqual(mesh.userdata.age, 23)
-        mesh.userdata.height = 1.68
-        self.assertEqual(mesh.userdata.height, 1.68)
+        mesh.metadata.name = 'Joe Bloggs'
+        self.assertEqual(mesh.metadata.name, 'Joe Bloggs')
+        mesh.metadata.age = 23
+        self.assertEqual(mesh.metadata.age, 23)
+        mesh.metadata.height = 1.68
+        self.assertEqual(mesh.metadata.height, 1.68)
         
     def test_get_dict(self):
         mesh = mesher.Mesh()
-        mesh.userdata.name = 'Joe Bloggs'
-        mesh.userdata.age = 23
-        mesh.userdata.height = 1.68
-        d = mesh.userdata.get_dict()
-        print d
+        mesh.metadata.name = 'Joe Bloggs'
+        mesh.metadata.age = 23
+        mesh.metadata.height = 1.68
+        d = mesh.metadata.get_dict()
+        
         self.assertEqual(d,
                 {'name':'Joe Bloggs', 'age':23, 'height':1.68})
     
     def test_set_dict(self):
         d = {'name':'Joe Bloggs', 'age':23, 'height':1.68}
         mesh = mesher.Mesh()
-        mesh.userdata.set_dict(d)
-        self.assertEqual(mesh.userdata.name, 'Joe Bloggs')
-        self.assertEqual(mesh.userdata.age, 23)
-        self.assertEqual(mesh.userdata.height, 1.68)
+        mesh.metadata.set_dict(d)
+        self.assertEqual(mesh.metadata.name, 'Joe Bloggs')
+        self.assertEqual(mesh.metadata.age, 23)
+        self.assertEqual(mesh.metadata.height, 1.68)
        
         
 if __name__ == "__main__":
